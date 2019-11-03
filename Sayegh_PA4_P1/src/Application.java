734ddfadad1a6893416623fc85282f77616aa5c6
Eric Sayegh <ericsayegh@knights.ucf.edu>
2019-11-02 23:10:11 -0400

    first commit

---
+++ Sayegh_PA4_P1/src/Application.java
@@ -0,0 +1,17 @@
+public class Application {
+
+    public static void main(String[] args){
+
+        DuplicateRemover rem = new DuplicateRemover();
+
+        rem.remove("problem1.txt");
+        rem.write("unique_words.txt")
+
+    }
+
+
+
+
+
+
+}
